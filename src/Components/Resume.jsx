18dd49658ff844749ddc8208f4ee254d6ca94d85
Muaz Ashraf <muazashrafnet@gmail.com>
2023-03-24 14:01:25 +0500

    change font to ubuntu and color to astro navy

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -7,6 +7,7 @@ import PhoneIphoneIcon from "@mui/icons-material/PhoneIphone";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import CustomCard from "./UI/CustomCard";
+import "typeface-ubuntu";
 
 const Resume = () => {
   const location = useLocation();
@@ -36,17 +37,23 @@ const Resume = () => {
             <Typography
               variant="h3"
               sx={{
-                color: "blue",
+                color: "#002D62",
               }}
               textAlign="left"
+              fontFamily={"Ubuntu"}
             >
               {formData.name}
             </Typography>
 
-            <Typography variant="h5" textAlign="left">
+            <Typography variant="h5" textAlign="left" fontFamily={"Ubuntu"}>
               {formData.title}
             </Typography>
-            <Typography variant="body1" textAlign="left" mt={3}>
+            <Typography
+              variant="body1"
+              textAlign="left"
+              mt={3}
+              fontFamily={"Ubuntu"}
+            >
               {formData.description}
             </Typography>
           </Stack>
@@ -55,7 +62,7 @@ const Resume = () => {
           direction={{ xs: "column", sm: "row" }}
           mt={3}
           justifyContent="space-evenly"
-          bgcolor="royalblue"
+          bgcolor="#002D62"
           color="white"
           p={1}
         >
@@ -66,7 +73,9 @@ const Resume = () => {
             justifyContent={"flex-start"}
           >
             <EmailIcon />
-            <Typography variant="h6">{formData.email}</Typography>
+            <Typography variant="h6" fontFamily={"Ubuntu"}>
+              {formData.email}
+            </Typography>
           </Stack>
           <Stack
             direction={"row"}
@@ -75,7 +84,9 @@ const Resume = () => {
             justifyContent={"flex-start"}
           >
             <PhoneIphoneIcon />
-            <Typography variant="h6">{formData.phone}</Typography>
+            <Typography variant="h6" fontFamily={"Ubuntu"}>
+              {formData.phone}
+            </Typography>
           </Stack>
           <Stack
             direction={"row"}
@@ -84,7 +95,9 @@ const Resume = () => {
             justifyContent={"flex-start"}
           >
             <LocationOnIcon />
-            <Typography variant="h6">{formData.location}</Typography>
+            <Typography variant="h6" fontFamily={"Ubuntu"}>
+              {formData.location}
+            </Typography>
           </Stack>
           <Stack
             direction={"row"}
@@ -93,25 +106,46 @@ const Resume = () => {
             justifyContent={"flex-start"}
           >
             <LinkedInIcon />
-            <Typography variant="h6">{formData.linkedIn}</Typography>
+            <Typography variant="h6" fontFamily={"Ubuntu"}>
+              {formData.linkedIn}
+            </Typography>
           </Stack>
         </Stack>
         <Grid container>
-          <Grid item xs={6} sm={6}>
+          <Grid item xs={6} sm={6} pl={10}>
             <Stack mt={3} key={"1"}>
-              <Typography variant="h4" fontWeight="bold">
+              <Typography
+                variant="h4"
+                fontWeight="bold"
+                color="#002D62"
+                fontFamily={"Ubuntu"}
+              >
                 EDUCATION
               </Typography>
               {formData.education.map((item, index) => {
                 return (
                   <>
-                    <Typography variant="h6" fontWeight="bold" key={index + 3}>
+                    <Typography
+                      variant="h6"
+                      fontWeight="bold"
+                      key={index + 3}
+                      fontFamily={"Ubuntu"}
+                    >
                       {item.degree}
                     </Typography>
-                    <Typography variant="h6" key={index + 4}>
+                    <Typography
+                      variant="h6"
+                      key={index + 4}
+                      fontFamily={"Ubuntu"}
+                    >
                       {item.university}
                     </Typography>
-                    <Typography variant="body1" key={index + 5}>
+                    <Typography
+                      variant="body1"
+                      key={index + 5}
+                      fontStyle="italic"
+                      fontFamily={"Ubuntu"}
+                    >
                       {item.yearEnrolled} - {item.yearCompleted}
                     </Typography>
                   </>
@@ -119,20 +153,38 @@ const Resume = () => {
               })}
             </Stack>
             <Stack mt={2} key={"2"}>
-              <Typography variant="h4" fontWeight="bold">
+              <Typography
+                variant="h4"
+                fontWeight="bold"
+                color="#002D62"
+                fontFamily={"Ubuntu"}
+              >
                 WORK EXPERIENCE
               </Typography>
 
               {formData.experience.map((item, index) => {
                 return (
                   <>
-                    <Typography variant="h6" fontWeight="bold" key={index + 6}>
+                    <Typography
+                      variant="h6"
+                      fontWeight="bold"
+                      key={index + 6}
+                      fontFamily={"Ubuntu"}
+                    >
                       {item.role}
                     </Typography>
-                    <Typography variant="h6" key={index + 7}>
+                    <Typography
+                      variant="h6"
+                      key={index + 7}
+                      fontFamily={"Ubuntu"}
+                    >
                       {item.company}
                     </Typography>
-                    <Typography variant="h6" key={index + 8}>
+                    <Typography
+                      variant="h6"
+                      key={index + 8}
+                      fontFamily={"Ubuntu"}
+                    >
                       {item.yearsCompany} years
                     </Typography>
                   </>
@@ -142,13 +194,20 @@ const Resume = () => {
           </Grid>
 
           <Grid item xs={12} sm={6}>
-            <Typography variant="h4" fontWeight="bold" mb={3} mt={3}>
+            <Typography
+              variant="h4"
+              fontWeight="bold"
+              mt={3}
+              color="#002D62"
+              fontFamily={"Ubuntu"}
+            >
               SKILLS
             </Typography>
             <Stack mt={3} direction={{ xs: "column", sm: "row" }} xs={4} sm={3}>
               {formData.skills.map((items, index) => {
                 return (
                   <Typography
+                    fontFamily={"Ubuntu"}
                     key={index + 1}
                     variant="h6"
                     textAlign="center"
@@ -157,7 +216,7 @@ const Resume = () => {
                       border: "1px solid black",
                       borderRadius: "0.5em",
                       padding: "0.4em",
-                      bgcolor: "royalblue",
+                      bgcolor: "#002D62",
                       color: "white",
                       margin: "0.2em",
                     }}
@@ -167,13 +226,20 @@ const Resume = () => {
                 );
               })}
             </Stack>
-            <Typography variant="h4" fontWeight="bold" mt={3}>
+            <Typography
+              variant="h4"
+              fontWeight="bold"
+              mt={3}
+              color="#002D62"
+              fontFamily={"Ubuntu"}
+            >
               INTERESTS
             </Typography>
             <Stack mt={10} direction={{ xs: "column", sm: "row" }} m="0.4em">
               {formData.interests.map((items, index) => {
                 return (
                   <Typography
+                    fontFamily={"Ubuntu"}
                     key={index + 2}
                     variant="h6"
                     textAlign="center"
